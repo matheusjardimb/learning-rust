90ddbff2faaf793e3688b18e5cb3176edf87657c
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-07-10 16:56:37 -0300

    04. enums

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    let get = Method::GET;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -17,4 +19,22 @@ impl Server {
     fn run(self) {
         println!("Listening to {}", self.address)
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    PATCH,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
 }
