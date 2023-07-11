92490d631803daf80233d1466377a33ae23d7627
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-07-10 21:14:51 -0300

    modules

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,14 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
-    let get = Method::GET;
+    // let get = Method::GET;
 
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    address: String,
-}
-
-impl Server {
-    fn new(address: String) -> Self {
-        Self {
-            address
-        }
-    }
-
-    fn run(self) {
-        println!("Listening to {}", self.address)
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    PATCH,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-}
