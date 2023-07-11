b373b12fac6a6ffc788177e9e3102db490b7e227
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-07-10 21:41:37 -0300

    creating TCP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,5 +10,12 @@ fn main() {
 
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
+
+    // Syntactic sugar of 'while true'
+    // 'outer: loop {
+    //     'inner: loop {
+    //         break 'outer;
+    //     }
+    // }
 }
 
