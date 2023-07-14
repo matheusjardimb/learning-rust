7d8e5264e0cbc9f95b0de15c78190c35571aff0a
Matheus Jardim <matheus.jardim@conceptu.ind.br>
2023-07-14 16:22:41 -0300

    add response html

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,11 +1,10 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
+use super::QueryString;
 use std::convert::TryFrom;
 use std::error::Error;
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str;
 use std::str::Utf8Error;
-use crate::http::method::MethodError;
-use crate::http::query_string::QueryString;
 
 #[derive(Debug)]
 pub struct Request<'buf> {
@@ -23,21 +22,22 @@ impl<'buf> Request<'buf> {
         &self.method
     }
 
-    // pub fn query_string(&self) -> Option<&QueryString> {
-    //     self.query_string.as_ref()
-    // }
+    pub fn query_string(&self) -> Option<&QueryString> {
+        self.query_string.as_ref()
+    }
 }
 
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
-        let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+        let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
@@ -49,6 +49,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
             query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i];
         }
+
         Ok(Self {
             path,
             query_string,
@@ -74,18 +75,6 @@ pub enum ParseError {
     InvalidMethod,
 }
 
-impl Debug for ParseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message())
-    }
-}
-
-impl Display for ParseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message())
-    }
-}
-
 impl ParseError {
     fn message(&self) -> &str {
         match self {
@@ -106,6 +95,18 @@ impl From<MethodError> for ParseError {
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
     }
 }
 
